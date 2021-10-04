1824189387e019641cb76464c45f79766f911329
Katya <konovalova.ep@prg-it.ru>
2021-10-04 13:55:54 +0500

    пробная

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var User = /** @class */ (function () {
-    function User(_name) {
+/* class User{
+    name : string;
+    constructor(_name:string){
+          
         this.name = _name;
     }
-    return User;
-}());
-var tom = new User("Том");
-var header = this.document.getElementById("header");
-header.innerHTML = "Привет " + tom.name;
+}
+const tom : User = new User("Том");
+const header = this.document.getElementById("header");
+header.innerHTML = "Привет " + tom.name; */
+
+/* const create: number=123
+console.log(create) */
+
+function sum(x, y) {
+    return x + y;
+}
+;
+function multiply(a, b) {
+    return a * b;
+}
+;
+function mathOp(x, y, op) {
+    return op(x, y);
+}
+console.log(mathOp(10, 20, sum)); // 30 
+console.log(mathOp(10, 20, multiply)); // 200 
--- app.ts
+++ app.ts
@@ -9,5 +9,19 @@ const tom : User = new User("Том");
 const header = this.document.getElementById("header");
 header.innerHTML = "Привет " + tom.name; */
 
-const num:number=123
-console.log(num)
+/* const create: number=123
+console.log(create) */
+
+function sum (x: number, y: number): number {
+    return x + y;
+};
+function multiply (a: number, b: number): number {
+    return a * b;
+};
+  
+function mathOp(x: number, y: number, op: (a: number, b: number) => number): number{
+  
+    return op(x, y);
+}
+console.log(mathOp(10, 20, sum)); // 30 
+console.log(mathOp(10, 20, multiply)); // 200 
